54713a83e1280209b97373696e7f7e431aae2c3d
Vanessa Heckman <vanessamheckman@gmail.com>
2014-07-01 23:10:10 -0700

    small fixes

--- app/views.py
+++ app/views.py
@@ -10,14 +10,7 @@ import csv
 import math
 import random
 
-# To create a database connection, add the following
-# within your view functions:
-# con = con_db(host, port, user, passwd, db)
-
-# ROUTING/VIEW FUNCTIONS
-#'''
-#@app.route('/')
-#'''
+# Define helper functions
 from app.helpers.database import con_db, query_db
 from app.helpers.filters import format_currency
 import jinja2
@@ -71,7 +64,7 @@ def hello():
 
 @app.route('/index.html')
 def index():
-    return render_template('index.html')#    # name = 'Vanessa Heckman'
+    return render_template('index.html')    
 
 @app.route("/about.html")
 def aboutfn():
@@ -80,122 +73,52 @@ def aboutfn():
 @app.route("/search.html")
 @requires_auth
 def search():
-    user = app.config["DATABASE_USER"]
-    host = app.config["DATABASE_HOST"]
-    port = app.config["DATABASE_PORT"]
-    pw   = app.config["DATABASE_PASSWORD"]
-    db   = app.config["DATABASE_DB"]
+    user   = app.config["DATABASE_USER"]
+    host   = app.config["DATABASE_HOST"]
+    port   = app.config["DATABASE_PORT"]
+    passwd = app.config["DATABASE_PASSWORD"]
+    db     = app.config["DATABASE_DB"]
     
     # Need to change this on AWS:
-    #con = MySQLdb.connect(user=user, host=host, port=port, pw = pw, db=db)
-    con = MySQLdb.connect(user=user, host=host, port=port, db=db, charset='utf8')
+    con = MySQLdb.connect(user=user, host=host, port=port, passwd = passwd, db=db)
     cursor = con.cursor()
 
     # Loan ID
     loan_id = int(request.args.get("loan_id", None))
     pickle.dump( loan_id, open( "passloan.pkl", "wb"))
-    
-    # Load loan info
-    loan_info = sqlExec("SELECT borrowerid, sift_score, interest, period, applydate-borrowers.Created as time_to_complete, firstname as first, reffered_by, Country as country, City as city, Amount as amount, loanuse from loanapplic join borrowers on loanapplic.borrowerid = borrowers.userid where loanid = %d;" % loan_id)
-    loan_info[0]['loanuse'] = remove_uni(loan_info[0]['loanuse'])
 
-    # Load borrower history
-    #loan_hist = sqlExec("SELECT loanid, Amount as amount FROM loanapplic WHERE borrowerid = %d AND loanid < %d;" % ( loan_info[0]['borrowerid'], loan_id))
-    loan_hist = sqlExec("SELECT count(loanid) as n_prev FROM loanapplic WHERE borrowerid = %d AND loanid < %d;" % (loan_info[0]['borrowerid'], loan_id))
-    
+    # Load loan info
+    loan_info = sqlExec("SELECT borrowerid, sift_score, interest, period, applydate-borrowers.Created as time_to_complete, firstname as first, reffered_by as referred_by, Country as country, City as city, Amount as amount, loanuse from loanapplic join borrowers on loanapplic.borrowerid = borrowers.userid where loanid = %d;" % loan_id)
+    print loan_info[0]['loanuse']
+    loan_info[0]['loanuse'] = loan_info[0]['loanuse'].decode("ascii", "ignore")
+
     # Sift score
-    sift_score = loan_info[0]['sift_score']
-    interest   = loan_info[0]['interest']
-    period     = str(loan_info[0]['period'])
+    sift_score       = loan_info[0]['sift_score']
+    interest         = loan_info[0]['interest']
+    period           = loan_info[0]['period']
+    referred_by      = len(loan_info[0]['referred_by'])
     time_to_complete = loan_info[0]['time_to_complete']
-    
+    KE               = (loan_info[0]['country'] == 'KE')*1
+    SN               = (loan_info[0]['country'] == 'SN')*1
+    other            = (1-KE)*(1-SN)
+
     # Features
-    xin = np.array([sift_score, 0.7, period, 60.0*60*24])
-    
+    xin = np.array([sift_score, referred_by, KE, SN, other, time_to_complete])
+
     # Load model - Lin regression
     clf = pickle.load( open('app/helpers/model.pkl', "rb"))
     y = clf.predict(xin)[0]
-    prob_fund = clf.predict_proba(xin)[0][1]
-
+    prob_default = clf.predict_proba(xin)[0][1]
+    
     # Make prediction
     if y == 0:
         ypred = 'Will pay back loan in full'
     elif y ==1:
         ypred = 'Will default on loan'
-
+    
     # Set image id file
     image_id_str = "static/images/loan_images/" + str(loan_id) + ".jpg"
-    return render_template('search.html',loan_id=loan_id, loan_info=loan_info, ypred=ypred, prob_fund=prob_fund, image_id_str =image_id_str, loan_hist = loan_hist)
-
-@app.route("/eval.html")
-def eval():
-    #loan_id = pickle.load( open( "passloan.pkl", "rb" ))
-    loan_id = 130
-    
-    loan_info=sqlExec("SELECT  borrowers_gender, location_country,location_country_code, sector, loan_amount, description_num_languages, posted_date, terms_repayment_term, image_id from Loans where loan_id = %d;" % loan_id)
-
-    loan_info=sqlExec("SELECT  COUNT(loanid), borrowers_gender, location_country,location_country_code, sector, loan_amount, description_num_languages, posted_date, terms_repayment_term, image_id from Loans where loan_id = %d;" % loan_id)
-
-    
-    posted_date_months=loan_info[0]['posted_date'].month
-
-    # change country code to continent
-    continent_map = pickle.load( open( "continent_map.pkl", "rb" ) )
-    continent = continent_map[loan_info[0]['location_country_code']]
-    #vectorize contients and sectors
-    (contdict,sectordict) = pickle.load( open( "contsect_dict.pkl", "rb" ) )
-    continent_vec = contdict[continent]
-    sector_vec = sectordict[loan_info[0]['sector']]
-    
-    if loan_info[0]['borrowers_gender'] == 'F':
-        borrowers_gender = 1
-    else:
-        borrowers_gender=0
-
-
-    clf = pickle.load( open( "model.pkl", "rb" ) )
-    
-    requested_loan_amount = loan_info[0]['loan_amount']
-    requested_repayment_term = loan_info[0]['terms_repayment_term']
-    binreqLoanAmount=int(round(loan_info[0]['loan_amount'],-2))/100
-
-    if requested_repayment_term > 10:
-        month_pred = [i for i in range(requested_repayment_term-10,requested_repayment_term+10)]
-    else:
-        month_pred = [(i+1) for i in range(20)]
-
-    roundedReqAmt = int(math.floor(requested_loan_amount/100))
-
-    if binreqLoanAmount >= 10:
-        gridAmtSet = binreqLoanAmount
-    else:
-        gridAmtSet = 5
-
-    amount_pred = [100*i for i in range(gridAmtSet-5,gridAmtSet+5)]
-    
-    #create list of tuples for (amount, month, funding prob )
-    predMatrix=[]
-    for amount in amount_pred:
-        for month in month_pred:
-            xin = np.append(continent_vec,sector_vec)
-            xin = np.append(xin,np.array( [borrowers_gender,loan_info[0]['description_num_languages'],amount,posted_date_months,month ] ))
-
-            predprob = round(clf.predict_proba(xin)[0][0],2)
-            predMatrix.append((amount, month, predprob) )
-    
-    # write to tsv file
-    ofile  = open('static/data.tsv', "wb")
-    writer = csv.writer(ofile,delimiter='\t')
-
-    writer.writerow(['day','hour', 'value'])
-    for row in predMatrix:
-                writer.writerow(row)
-    ofile.close()
-
-    amount_pred_str = str(amount_pred)
-    month_pred_str = str(month_pred)
-
-    return render_template('eval.html',loan_info=loan_info,loan_id=loan_id,roundedReqAmt=roundedReqAmt,requested_repayment_term=requested_repayment_term,gridAmtSet=gridAmtSet,amount_pred_str=amount_pred_str,month_pred_str=month_pred_str,binreqLoanAmount=binreqLoanAmount)
+    return render_template('search.html', loan_id=loan_id, loan_info=loan_info, ypred=ypred, prob_default=prob_default, image_id_str =image_id_str)
 
 @app.route("/blocker")
 def block():
